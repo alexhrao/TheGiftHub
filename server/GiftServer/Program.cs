00e93a378c470f2bbe1fae519c7380a88d22b236
Alexander Rao <alexhrao@gmail.com>
2017-07-23 14:11:56 -0400

    Reconfigure to use .NET framework (instead of CORE)

--- server/GiftServer/Program.cs
+++ server/GiftServer/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace GiftServer
 {
@@ -6,7 +10,6 @@ namespace GiftServer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
