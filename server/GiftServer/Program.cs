f19ea8c95e2069a3558885057a2e7b8b762a8c68
Alexander Rao <alexhrao@gmail.com>
2017-07-23 11:52:18 -0400

    Initial Visual Studio Setup

---
+++ server/GiftServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GiftServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
