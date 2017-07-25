3b1c70c5c856c92f8430e9ea01c7b491c514b0b8
Alexander Rao <alexhrao@gmail.com>
2017-07-25 16:29:13 -0400

    Allow Dispatch to access request & response

--- server/GiftServer/Program.cs
+++ server/GiftServer/Program.cs
@@ -1,51 +1,18 @@
 ﻿using System;
 using System.Net;
-using System.Threading;
-using System.Text;
 using System.IO;
 
 namespace GiftServer
 {
-    public class WebServer
+    public class Program
     {
-        private readonly HttpListener _listener = new HttpListener();
-        private readonly Func<HttpListenerRequest, string> _responseGenerator;
-        
-        public WebServer(string prefixes, Func<HttpListenerRequest, string> method)
-        {
-            _listener.Prefixes.Add(prefixes);
-            _responseGenerator = method;
-            _listener.Start();
-        }
-        public void Run()
-        {
-            ThreadPool.QueueUserWorkItem((obj) =>
-            {
-                while (_listener.IsListening)
-                {
-                    ThreadPool.QueueUserWorkItem((r) =>
-                    {
-                        HttpListenerContext rtx = (HttpListenerContext)(r);
-                        string resp = _responseGenerator(rtx.Request);
-                        byte[] respBuffer = Encoding.UTF8.GetBytes(resp);
-                        rtx.Response.ContentLength64 = respBuffer.Length;
-                        rtx.Response.OutputStream.Write(respBuffer, 0, respBuffer.Length);
-                        rtx.Response.OutputStream.Close();
-                    }, _listener.GetContext());
-                }
-            });
-        }
-        public void Stop()
-        {
-            _listener.Stop();
-            _listener.Close();
-        }
-    }
-    class Program
-    {
+        /// <summary>
+        /// Main method and entry point - starts the web server on the specified host/port.
+        /// </summary>
+        /// <param name="args">Reserved for future use</param>
         public static void Main(string[] args)
         {
-            WebServer server = new WebServer("http://localhost:60001/", Dispatch);
+            Server.WebServer server = new Server.WebServer("http://localhost:60001/", Dispatch);
             server.Run();
             Console.WriteLine("Server is Active...\nType quit or q to quit");
             string input = null;
@@ -63,9 +30,11 @@ namespace GiftServer
         /// </summary>
         /// <param name="request">The incoming HTML request, in it's entirety</param>
         /// <returns></returns>
-        public static string Dispatch(HttpListenerRequest request)
+        public static string Dispatch(HttpListenerContext rtx)
         {
             // Check if user is logged in (via cookies?)
+            HttpListenerRequest request = rtx.Request;
+            HttpListenerResponse response = rtx.Response;
             bool isLoggedIn = false;
             if (request.HasEntityBody)
             {
@@ -77,11 +46,13 @@ namespace GiftServer
                     Console.Write(input);
                 }
                 return "<html><body><form method=\"POST\"><input name=\"theMail\" type=\"email\"/><button type=\"submit\" value=\"submit\">Hello</button></form></body></html>";
-            } else if (isLoggedIn)
+            }
+            else if (isLoggedIn)
             {
                 // If logged in (but no request), just send back home page:
                 return "<html><body><form method=\"POST\"><input name=\"theMail\" type=\"email\"/><button type=\"submit\" value=\"submit\">Hello</button></form></body></html>";
-            } else
+            }
+            else
             {
                 // If not logged in, send the login page!
                 return "<html><body><form method=\"POST\"><input name=\"theMail\" type=\"email\"/><button type=\"submit\" value=\"submit\">Hello</button></form></body></html>";
---
+++ server/GiftServer/WebServer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Text;
+using System.IO;
+
+namespace GiftServer
+{
+    namespace Server
+    {
+        public class WebServer
+        {
+            private readonly HttpListener _listener = new HttpListener();
+            private readonly Func<HttpListenerContext, string> _responseGenerator;
+
+            public WebServer(string prefixes, Func<HttpListenerContext, string> method)
+            {
+                _listener.Prefixes.Add(prefixes);
+                _responseGenerator = method;
+                _listener.Start();
+            }
+            public void Run()
+            {
+                ThreadPool.QueueUserWorkItem((obj) =>
+                {
+                    while (_listener.IsListening)
+                    {
+                        ThreadPool.QueueUserWorkItem((r) =>
+                        {
+                            HttpListenerContext rtx = (HttpListenerContext)(r);
+                            string resp = _responseGenerator(rtx);
+                            byte[] respBuffer = Encoding.UTF8.GetBytes(resp);
+                            rtx.Response.ContentLength64 = respBuffer.Length;
+                            rtx.Response.OutputStream.Write(respBuffer, 0, respBuffer.Length);
+                            rtx.Response.OutputStream.Close();
+                        }, _listener.GetContext());
+                    }
+                });
+            }
+            public void Stop()
+            {
+                _listener.Stop();
+                _listener.Close();
+            }
+        }
+    }
+}
