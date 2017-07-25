01ac28613647805dfbcc339784a8c46c0dc4c2ca
Alexander Rao <alexhrao@gmail.com>
2017-07-25 15:52:36 -0400

    Add more documentation

--- server/GiftServer/PasswordHash.cs
+++ server/GiftServer/PasswordHash.cs
@@ -14,30 +14,28 @@ namespace GiftServer
             public const int SALT_SIZE = 16;
             public const int HASH_SIZE = 20;
             public const int HASH_ITER = 10000;
-            private readonly byte[] _salt;
-            private readonly byte[] _hash;
-            private static readonly RNGCryptoServiceProvider rand = new RNGCryptoServiceProvider();
+            private readonly byte[] _salt = new byte[SALT_SIZE];
+            private readonly byte[] _hash = new byte[HASH_SIZE];
+            private readonly RNGCryptoServiceProvider rand = new RNGCryptoServiceProvider();
 
             /// <summary>
             /// Create a PasswordHash object from a password, which can then be used to retreive secure password hashes.
             /// </summary>
-            /// <param name="password"></param>
+            /// <param name="password">The password you want hashed</param>
             public PasswordHash(string password)
             {
-                _salt = new byte[SALT_SIZE];
                 rand.GetBytes(_salt);
                 _hash = new Rfc2898DeriveBytes(password, _salt, HASH_ITER).GetBytes(HASH_SIZE);
             }
             /// <summary>
             /// Create a PasswordHash object from an existing hash - useful for comparison & verification
             /// </summary>
-            /// <param name="hashBytes"></param>
+            /// <param name="hashBytes">The hashed password you want to box</param>
             public PasswordHash(byte[] hashBytes)
             {
                 Array.Copy(hashBytes, 0, _salt = new byte[SALT_SIZE], 0, SALT_SIZE);
                 Array.Copy(hashBytes, SALT_SIZE, _hash = new byte[HASH_SIZE], 0, HASH_SIZE);
             }
-
             /// <summary>
             /// Get the salted & hashed password from this PasswordHash
             /// </summary>
@@ -52,7 +50,7 @@ namespace GiftServer
             /// <summary>
             /// Verify that the input password is indeed the same as the stored password.
             /// </summary>
-            /// <param name="password"></param>
+            /// <param name="password">The password you'd like to verify</param>
             /// <returns>True if the passwords match; false otherwise</returns>
             public bool Verify(string password)
             {
