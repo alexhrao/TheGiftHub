e6a3fc4c1093343ea8015d753e908171a19c3926
Alexander Rao <alexhrao@gmail.com>
2017-07-27 17:35:31 -0400

    Edit HTML and SQL

--- database/CR02Users.sql
+++ database/CR02Users.sql
@@ -5,7 +5,9 @@ CREATE TABLE gift_registry_db.users (
     PasswordID INT NOT NULL,
     UserEmail VARCHAR(255) NOT NULL UNIQUE,
     UserTheme TINYINT UNSIGNED DEFAULT 0,
-    UserImage VARCHAR(255) NULL
+    UserImage VARCHAR(255) NOT NULL DEFAULT "default.png",
+    DateOfBirth DATE NULL,
+    TimeCreated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 ALTER TABLE gift_registry_db.users
