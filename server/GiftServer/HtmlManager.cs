f789e5f0a4b397a4d50bbe931114dd8d2cbb5109
Alexander Rao <alexhrao@gmail.com>
2017-07-29 17:43:17 -0400

    Fix alert

--- server/GiftServer/HtmlManager.cs
+++ server/GiftServer/HtmlManager.cs
@@ -14,6 +14,8 @@ namespace GiftServer
                 login.LoadHtml(Resources.header + Resources.login);
                 HtmlNode alert = login.DocumentNode.SelectSingleNode("//*[contains(concat(\" \", normalize-space(@class), \" \"), \" alert \")]");
                 alert.AddClass("alert-danger");
+                alert.AddClass("in");
+                alert.RemoveClass("hidden");
                 HtmlNode message = HtmlNode.CreateNode("<p><strong>Uh-Oh...</strong> Looks like we didn't recognize that Username/Password pair."
                                                      + "Try again or, <a data-toggle=\"modal\" href=\"#resetPassword\">Reset your Password</a></p>");
                 HtmlNodeCollection children = new HtmlNodeCollection(alert);
@@ -27,6 +29,8 @@ namespace GiftServer
                 login.LoadHtml(Resources.header + Resources.login);
                 HtmlNode alert = login.DocumentNode.SelectSingleNode("//*[contains(concat(\" \", normalize-space(@class), \" \"), \" alert \")]");
                 alert.AddClass("alert-success");
+                alert.AddClass("in");
+                alert.RemoveClass("hidden");
                 HtmlNode message = HtmlNode.CreateNode("<p><strong>Success!</strong> Please login below</p>");
                 HtmlNodeCollection children = new HtmlNodeCollection(alert);
                 children.Add(message);
@@ -39,6 +43,8 @@ namespace GiftServer
                 login.LoadHtml(Resources.header + Resources.login);
                 HtmlNode alert = login.DocumentNode.SelectSingleNode("//*[contains(concat(\" \", normalize-space(@class), \" \"), \" alert \")]");
                 alert.AddClass("alert-success");
+                alert.AddClass("in");
+                alert.RemoveClass("hidden");
                 HtmlNode message = HtmlNode.CreateNode("<p><strong>Recovery email sent</strong> - check your inbox</p>");
                 HtmlNodeCollection children = new HtmlNodeCollection(alert);
                 children.Add(message);
@@ -51,6 +57,8 @@ namespace GiftServer
                 login.LoadHtml(Resources.header + Resources.login);
                 HtmlNode alert = login.DocumentNode.SelectSingleNode("//*[contains(concat(\" \", normalize-space(@class), \" \"), \" alert \")]");
                 alert.AddClass("alert-success");
+                alert.AddClass("in");
+                alert.RemoveClass("hidden");
                 HtmlNode message = HtmlNode.CreateNode("<p><strong>Password Reset</strong> Please login below with your new password</p>");
                 HtmlNodeCollection children = new HtmlNodeCollection(alert);
                 children.Add(message);
