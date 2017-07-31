fce486f7bbffad406d1a3f60eb8ddf2dca074017
Alexander Rao <alexhrao@gmail.com>
2017-07-31 16:17:02 -0400

    Add skeleton for unit tests

---
+++ server/GiftServerTester/ResetManagerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GiftServerTester
+{
+    [TestClass]
+    public class ResetManagerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
