faa9ea786c3384bfbab65dfe5f1cc8f7792c9448
Alexander Rao <alexhrao@gmail.com>
2017-08-01 10:53:39 -0400

    Add NavigationManager; improve EventManager

--- server/GiftServer/DashboardManager.cs
+++ server/GiftServer/DashboardManager.cs
@@ -24,7 +24,36 @@ namespace GiftServer
                         cmd.Connection = con;
                         // Get all events that have anything to do with anyone in any group with our user.
                         // look at template
-
+                        cmd.CommandText = "SELECT eventsusersgroups.EventUserID "
+                                        + "FROM eventsusersgroups "
+                                        + "INNER JOIN eventsusers ON eventsusersgroups.EventUserID = eventsusers.EventUserID "
+                                        + "WHERE eventsusers.UserID IN ( "
+                                            + "SELECT UserID "
+                                            + "FROM gift_registry_db.groupsusers "
+                                            + "WHERE GroupID IN ( "
+                                                + "SELECT GroupID "
+                                                + "FROM gift_registry_db.groupsusers "
+                                                + "WHERE gift_registry_db.groupsusers.UserID = @uid "
+                                            + ") "
+                                        + ") "
+                                        + "AND eventsusersgroups.GroupID IN ( "
+                                            + "SELECT GroupID "
+                                            + "FROM gift_registry_db.groupsusers "
+                                            + "WHERE GroupID IN ( "
+                                                + "SELECT GroupID "
+                                                + "FROM gift_registry_db.groupsusers "
+                                                + "WHERE gift_registry_db.groupsusers.UserID = @uid "
+                                            + ") "
+                                        + ");";
+                        cmd.Parameters.AddWithValue("@uid", userID);
+                        cmd.Prepare();
+                        using (MySqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                // Add HtmlNode to our document?
+                            }
+                        }
                     }
                 }
                 return page;
