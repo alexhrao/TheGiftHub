2e637593a32c282df4b5798175df29a224bfdca9
Alexander Rao <alexhrao@gmail.com>
2018-02-03 16:05:14 -0500

    Add Basic Testing Framework

---
+++ database/load/LD00_Test.sql
@@ -0,0 +1,109 @@
+USE gift_registry_db_test;
+
+-- Create categories:
+INSERT INTO categories (CategoryName, CategoryDescription)
+    VALUES ('Clothing', 'Clothing');
+INSERT INTO categories (CategoryName, CategoryDescription)
+    VALUES ('Electronics', 'Electronic Devices');
+
+-- Create users:
+INSERT INTO users (UserName, UserEmail, UserBirthMonth, UserBirthDay, UserBio, UserURL)
+    VALUES ('Alex Rao', 'alexhrao@gmail.com', 7, 3, "I LOVE to have fun!", "UJIOl8xUU7ccIQH5Ofs0Awfdsa00");
+INSERT INTO users (UserName, UserEmail, UserBirthMonth, UserBirthDay, UserBio, UserURL)
+    VALUES ('Raeedah Choudhury', 'raeedah.choudhury@gmail.com', 11, 19, "I love my boyfriend", "UJIOl8xUU7ccIQH5Ofs0Awfdsa11");
+
+-- Create preferences:
+INSERT INTO preferences (UserID, UserCulture, UserTheme)
+    VALUES (1, 'en-US', 0);
+INSERT INTO preferences (UserID, UserCulture, UserTheme)
+    VALUES (2, 'fr-FR', 1);
+    
+-- Create Password:
+INSERT INTO passwords (UserID, PasswordHash, PasswordSalt, PasswordIter)
+    VALUES (1, 'xITVQ4AJ9eLfuzeG+cxeajH3QRw=', 'lN6ivSpluwewZWZCVXFNBw==', 10000);
+
+INSERT INTO passwords (UserID, PasswordHash, PasswordSalt, PasswordIter)
+    VALUES (2, '12345678901234567890', '1234567890123567', 10000);
+
+-- Create Events
+INSERT INTO user_events (UserID, EventStartDate, EventEndDate, EventName)
+	VALUES (1, '2017-01-10', NULL, 'Exact Event Test');
+INSERT INTO user_events (UserID, EventStartDate, EventEndDate, EventName)
+	VALUES (1, '2017-01-7', '2020-01-01', 'Relative Event Test');
+INSERT INTO user_events (UserID, EventStartDate, EventEndDate, EventName)
+	VALUES (1, '2017-08-04', '2017-08-04', 'One Off Event Test');
+INSERT INTO user_events (UserID, EventStartDate, EventEndDate, EventName)
+	VALUES (1, '2017-01-05', NULL, 'Blackout Date Test');
+INSERT INTO user_events (UserID, EventStartDate, EventEndDate, EventName)
+	VALUES (2, '2017-01-10', NULL, 'Exact Event Test 2');
+INSERT INTO user_events (UserID, EventStartDate, EventEndDate, EventName)
+	VALUES (2, '2017-01-7', '2020-01-01', 'Relative Event Test 2');
+INSERT INTO user_events (UserID, EventStartDate, EventEndDate, EventName)
+	VALUES (2, '2017-08-04', '2017-08-04', 'One Off Event Test 2');
+INSERT INTO user_events (UserID, EventStartDate, EventEndDate, EventName)
+	VALUES (2, '2017-04-04', NULL, 'Blackout Date Test 2');
+
+-- Create groups
+INSERT INTO groups (GroupName, AdminID)
+    VALUES ('The Rao''s', 1);
+INSERT INTO groups (GroupName, AdminID)
+	VALUES ('Test Group 2', 2);
+    
+-- Create gift
+INSERT INTO gifts (UserID, GiftName, GiftDescription, GiftURL, GiftCost, GiftStores, GiftQuantity, GiftColor, GiftColorText, GiftSize, CategoryID, GiftRating)
+    VALUES (1, 'Webcam', 'Logitech BRIO webcam', 'https://www.google.com', 100.00, 'Logitech, Target', 1, 'FFFFFF', 'White', 'N/A', 2, 5.0);
+INSERT INTO gifts (UserID, GiftName, GiftDescription, GiftURL, GiftCost, GiftStores, GiftQuantity, GiftColor, GiftColorText, GiftSize, CategoryID, GiftRating)
+    VALUES (2, 'Wav', 'WavDeWav', 'https://www.google.com', 100000.00, 'Target, Target', 1000, '000000', 'black', 'BIG', 1, 5.0);
+
+-- Add user to group
+INSERT INTO groups_users (GroupID, UserID)
+    VALUES (1, 1);
+INSERT INTO groups_users (GroupID, UserID, IsChild)
+    VALUES (1, 2, TRUE);
+
+-- Add gift to group
+INSERT INTO groups_gifts (GroupID, GiftID)
+    VALUES (1, 1);
+INSERT INTO groups_gifts (GroupID, GiftID)
+    VALUES (1, 2);
+
+-- Create the Exact Event Rule
+INSERT INTO exact_events (EventID, EventTimeInterval, EventSkipEvery)
+	VALUES (1, 'M', 1);
+INSERT INTO exact_events (EventID, EventTimeInterval, EventSkipEvery)
+	VALUES (5, 'W', 3);
+INSERT INTO exact_events (EventID, EventTimeInterval, EventSkipEvery)
+	VALUES (4, 'M', 1);
+    
+-- Create the Relative Event Rule
+INSERT INTO relative_events (EventID, EventTimeInterval, EventSkipEvery, EventDayOfWeek, EventPosn)
+	VALUES (2, 'MAR', 1, 'M', 2);
+INSERT INTO relative_events (EventID, EventTimeInterval, EventSkipEvery, EventDayOfWeek, EventPosn)
+	VALUES (6, NULL, 2, 'S', 3);
+INSERT INTO relative_events (EventID, EventTimeInterval, EventSkipEvery, EventDayOfWeek, EventPosn)
+	VALUES (8, NULL, 1, 'T', 5);
+
+-- Create Blackout Dates
+INSERT INTO blackout_events (EventID, EventBlackoutDate)
+	VALUES (4, '2017-02-05');
+INSERT INTO blackout_events (EventID, EventBlackoutDate)
+	VALUES (8, '2017-06-27');
+
+-- Add events to groups
+INSERT INTO groups_events (GroupID, EventID)
+	VALUES
+		(1, 1),
+        (1, 2),
+        (1, 3),
+        (2, 4),
+        (1, 5),
+        (2, 6),
+        (2, 7),
+        (2, 8);
+    
+-- Add Cultures
+INSERT INTO cultures (CultureLanguage, CultureLocation, CultureDesc)
+    VALUES
+    ('en', 'US', 'English (United States)'),
+    ('fr', 'FR', 'French'),
+    ('en', 'GB', 'English (United Kingdom)');
--- server/GiftServerTests/UserTests.cs
+++ server/GiftServerTests/UserTests.cs
@@ -1,14 +1,90 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GiftServer.Data;
+using GiftServer.Exceptions;
+using System.Net.Mail;
+using GiftServer.Security;
 
 namespace GiftServerTests
 {
     [TestClass]
     public class UserTests
     {
+        [TestCategory("User"), TestCategory("Instantiate"), TestCategory("ExceptionThrown")]
         [TestMethod]
-        public void TestMethod1()
+        [ExpectedException(typeof(UserNotFoundException))]
+        public void UserInstantiate_ZeroID_ExceptionThrown()
         {
+            User user = new User(0);
+        }
+
+        [TestCategory("User"), TestCategory("Instantiate"), TestCategory("ExceptionThrown")]
+        [TestMethod]
+        [ExpectedException(typeof(UserNotFoundException))]
+        public void UserInstantiate_InvalidID_ExceptionThrown()
+        {
+            // User should not exist
+            User user = new User(10);
+        }
+
+        [TestCategory("User"), TestCategory("Instantiate"), TestCategory("ExceptionThrown")]
+        [TestMethod]
+        [ExpectedException(typeof(UserNotFoundException))]
+        public void UserInstantiate_InvalidHash_ExceptionThrown()
+        {
+            User user = new User("123445");
+        }
+
+        [TestCategory("User"), TestCategory("Instantiate"), TestCategory("ExceptionThrown")]
+        [TestMethod]
+        [ExpectedException(typeof(UserNotFoundException))]
+        public void UserInstantiate_InvalidEmail_ExceptionThrown()
+        {
+            User user = new User(new MailAddress("myTest@hotmail.com"));
+        }
+
+        [TestCategory("User"), TestCategory("Instantiate"), TestCategory("ExceptionThrown")]
+        [TestMethod]
+        [ExpectedException(typeof(InvalidPasswordException))]
+        public void UserInstantiate_InvalidPassword_ExceptionThrown()
+        {
+            User user = new User(new MailAddress("alexhrao@gmail.com"), "HelloWorld");
+        }
+
+        [TestCategory("User"), TestCategory("Instantiate"), TestCategory("Successful")]
+        [TestMethod]
+        public void UserInstantiate_ValidCredentials_NewUser()
+        {
+            User user = new User(new MailAddress("alexhrao@gmail.com"), "MyPassword");
+            Assert.IsNotNull(user, "Valid User was not initialized");
+            Assert.AreEqual("Alex Rao", user.UserName, "Incorrect User was fetched!");
+        }
+
+        [TestCategory("User"), TestCategory("Create"), TestCategory("ExceptionThrown")]
+        [TestMethod]
+        [ExpectedException(typeof(DuplicateUserException))]
+        public void UserCreate_DuplicateEmail_ExceptionThrown()
+        {
+            User user = new User(new MailAddress("alexhrao@gmail.com"), new Password("HelloWorld123"));
+            user.Create();
+        }
+
+        [TestCategory("User"), TestCategory("Create"), TestCategory("Successful")]
+        [TestMethod]
+        public void UserCreate_ValidData_NewUser()
+        {
+            User user = new User(new MailAddress("alexhrao@hotmail.com"), new Password("HelloWorld123"));
+            bool res = user.Create();
+            Assert.IsTrue(res, "Valid Create() returned false");
+            Assert.AreNotEqual(user.UserId, 0, "UserID was not updated after creation");
+        }
+
+        [ClassCleanup]
+        public static void Cleanup()
+        {
+            // Delete user alexhrao@hotmail.com
+            User user = new User(new MailAddress("alexhrao@hotmail.com"));
+            user.Delete();
         }
     }
 }
