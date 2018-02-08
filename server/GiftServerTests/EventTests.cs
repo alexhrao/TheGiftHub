8d84371bf8961c433638dbc3aadc53ca5ae13f2d
Alexander Rao <alexhrao@gmail.com>
2018-02-08 16:16:23 -0500

    Start work on EventTests

---
+++ server/GiftServerTests/EventTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GiftServerTests
+{
+    [TestClass]
+    public class EventTests
+    {
+        [TestCategory("Event")]
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
